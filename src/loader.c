7c52dc97a249bab6b845ea896bebff30be4b68e2
Lnevx <lnevx@protonmail.com>
2025-01-03 21:24:29 +0300

    Copy support of static elfs (MAP_FIXED replaces old pages)

--- src/loader.c
+++ src/loader.c
@@ -13,16 +13,12 @@
 #define PFLAGS(x)                                                     \
     ((((x) & PF_R) ? PROT_READ : 0) | (((x) & PF_W) ? PROT_WRITE : 0) \
      | (((x) & PF_X) ? PROT_EXEC : 0))
-#define LOAD_ERR   ((void *)-1)
-#define STACK_SIZE 0x21000
 
 extern char __stop_text[];
 
-static void *loadelf_anon(void *elf_addr, Elf_Ehdr *ehdr, Elf_Phdr *phdr) {
-    size_t minva, maxva;
-
-    minva = (size_t)-1;
-    maxva = 0;
+static void *loadelf(void *elf_addr, Elf_Ehdr *ehdr, Elf_Phdr *phdr) {
+    int flags, dyn = ehdr->e_type == ET_DYN;
+    size_t minva = -1, maxva = 0;
 
     for (Elf_Phdr *iter = phdr; iter < &phdr[ehdr->e_phnum]; iter++) {
         if (iter->p_type != PT_LOAD) continue;
@@ -33,11 +29,13 @@ static void *loadelf_anon(void *elf_addr, Elf_Ehdr *ehdr, Elf_Phdr *phdr) {
     minva = TRUNC_PG(minva);
     maxva = ROUND_PG(maxva);
 
-    void *base = s_mmap(0, maxva - minva, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (base == (void *)-1) return LOAD_ERR;
+    void *hint = dyn ? NULL : (void *)minva;
+    flags = dyn ? 0 : MAP_FIXED;
+    flags |= MAP_ANONYMOUS | MAP_PRIVATE;
+
+    void *base = s_mmap(hint, maxva - minva, PROT_NONE, flags, -1, 0);
     s_munmap(base, maxva - minva);
 
-    int flags = MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE;
     /* Now map each segment separately in precalculated address. */
     for (Elf_Phdr *iter = phdr; iter < &phdr[ehdr->e_phnum]; iter++) {
         size_t off;
@@ -45,20 +43,15 @@ static void *loadelf_anon(void *elf_addr, Elf_Ehdr *ehdr, Elf_Phdr *phdr) {
         if (iter->p_type != PT_LOAD) continue;
 
         off = iter->p_vaddr & ALIGN;
-        void *start = base + TRUNC_PG(iter->p_vaddr);
+        void *start = (dyn ? base : 0) + TRUNC_PG(iter->p_vaddr);
         ssize_t sz = ROUND_PG(iter->p_memsz + off);
 
         void *p = s_mmap(start, sz, PROT_WRITE, flags, -1, 0);
-        if (p == (void *)-1) goto err;
-
         memcpy(p + off, elf_addr + iter->p_offset, iter->p_filesz);
         s_mprotect(p, sz, PFLAGS(iter->p_flags));
     }
 
     return base;
-err:
-    s_munmap(base, maxva - minva);
-    return LOAD_ERR;
 }
 
 void fini() {
@@ -67,18 +60,21 @@ void fini() {
 
 __attribute__((section(".text._start"))) void _start() {
     void *elf_addr;
-    Elf_Ehdr *ehdr;
-    Elf_Phdr *phdr, *iter;
+    Elf_Ehdr *ehdr = elf_addr = &__stop_text;
+    Elf_Phdr *phdr = elf_addr + ehdr->e_phoff;
 
-    ehdr = elf_addr = &__stop_text;
-    phdr = elf_addr + ehdr->e_phoff;
+    void *base = loadelf(elf_addr, ehdr, phdr);
+    void (*entry)(void) = ehdr->e_entry + (ehdr->e_type == ET_DYN ? base : 0);
 
-    void *base = loadelf_anon(elf_addr, ehdr, phdr);
-    void (*entry)(void) = ehdr->e_entry + base;
-
-    Elf_auxv_t av[2];
-    av[0] = (Elf_auxv_t){AT_RANDOM, (unsigned long)base};
-    av[1] = (Elf_auxv_t){AT_NULL, 0};
+    Elf_auxv_t av[8];
+    av[0] = (Elf_auxv_t){AT_PHDR, (size_t)phdr};
+    av[1] = (Elf_auxv_t){AT_PHENT, (size_t)ehdr->e_phentsize};
+    av[2] = (Elf_auxv_t){AT_PHNUM, (size_t)ehdr->e_phnum};
+    av[3] = (Elf_auxv_t){AT_PAGESZ, PAGE_SIZE};
+    av[4] = (Elf_auxv_t){AT_ENTRY, (size_t)entry};
+    av[5] = (Elf_auxv_t){AT_CLKTCK, 100};
+    av[6] = (Elf_auxv_t){AT_RANDOM, (size_t)&_start};
+    av[7] = (Elf_auxv_t){AT_NULL, 0};
 
     a_trampo(entry, &av, sizeof(av), &fini);
 }
